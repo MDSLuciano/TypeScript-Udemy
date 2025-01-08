1d8d2fb5bea9e6d136756506f0ea540d30612130
Luciano Machado <lucianoomachados@gmail.com>
2025-01-07 22:12:59 -0300

    type array (typescript)

--- src/A04-type-array/index.ts
+++ src/A04-type-array/index.ts
@@ -1,1 +1,16 @@
+// Array<T> - T[]
+export function multiplyArgs(...args: Array<number>): number {
+  return args.reduce((acc, value) => acc * value, 1);
+}
+export function addStrings(...args: string[]): string {
+  return args.reduce((acc, value) => acc + value);
+}
 
+export function toUpperCase(...args: string[]): string[]{
+  return args.map(value => value.toUpperCase());
+}
+
+const result = multiplyArgs(1, 2, 3, 4, 5);
+const concatenations = addStrings('Luciano', ' ', 'Silva');
+const upper = toUpperCase("Noia")
+console.log(result, concatenations, upper);

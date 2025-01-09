3ce0ead2a3d8ce15dc68943b1c438a7d4c5d300b
Luciano Machado <lucianoomachados@gmail.com>
2025-01-09 18:21:44 -0300

    structural typing

---
+++ src/A15-structural-typing/index.ts
@@ -0,0 +1,18 @@
+type VerifyUser = (user: User, sentValue: User) => boolean;
+
+type User = {
+  username: string;
+  password: string;
+};
+
+const verifyUser: VerifyUser = (user, sentValue) => {
+  return (
+    user.username === sentValue.username &&
+    user.password === sentValue.password
+  );
+};
+
+const bdUser = { username: 'Luciano', password: '123456' };
+const sentUser = { username: 'Luciano', password: '123456' };
+const loggendIn = verifyUser(bdUser, sentUser);
+console.log(loggendIn);

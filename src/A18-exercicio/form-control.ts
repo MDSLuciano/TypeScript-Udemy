75282577ec7beaf86eac2a1258c4ab9edd8bd72c
Luciano Machado <lucianoomachados@gmail.com>
2025-01-11 16:14:52 -0300

    EXERCISE FORM WITH TYPESCRIPT

---
+++ src/A18-exercicio/form-control.ts
@@ -0,0 +1,56 @@
+import isEmail from 'validator/lib/isEmail';
+const SHOW_ERROR_MESSAGE = 'show-error-message';
+
+const form = document.getElementById('form') as HTMLFormElement;
+const userName = document.getElementById('username') as HTMLInputElement;
+const email = document.getElementById('email') as HTMLInputElement;
+const password = document.getElementById('password') as HTMLInputElement;
+const password2 = document.getElementById('password2') as HTMLInputElement;
+
+form.addEventListener('submit', function (event: Event) {
+  event.preventDefault();
+  hideErrorMessage(this);
+  checkForEmptyFields(userName, email, password, password2);
+  checkEmail(email);
+  checkEqualPassword(password, password2);
+  if (shouldSendForm(this)) console.log('FORMULÁRIO ENVIADO');
+});
+
+function checkForEmptyFields(...inputs: HTMLInputElement[]): void {
+  inputs.forEach((input) => {
+    if (!input.value) showErrorMessage(input, 'Campo não pode ficar vazio');
+  });
+}
+
+function checkEmail(input: HTMLInputElement): void {
+  if (!isEmail(input.value)) showErrorMessage(input, 'Email inválido');
+}
+
+function checkEqualPassword(
+  password: HTMLInputElement,
+  password2: HTMLInputElement,
+) {
+  if (password.value !== password2.value) {
+    showErrorMessage(password, 'As senhas são diferentes');
+    showErrorMessage(password2, 'As senhas são diferentes');
+  }
+}
+function hideErrorMessage(form: HTMLFormElement): void {
+  form
+    .querySelectorAll(`.${SHOW_ERROR_MESSAGE}`)
+    .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGE));
+}
+function showErrorMessage(input: HTMLInputElement, msg: string): void {
+  const formFields = input.parentElement as HTMLDivElement;
+  const errorMessage = formFields.querySelector(
+    '.error-message',
+  ) as HTMLSpanElement;
+  errorMessage.innerText = msg;
+  formFields.classList.add(SHOW_ERROR_MESSAGE);
+}
+
+function shouldSendForm(form: HTMLFormElement): boolean {
+  let send = true;
+  form.querySelectorAll('.' + SHOW_ERROR_MESSAGE).forEach(() => (send = false));
+  return send;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/A0019-exercicio/A0019-exercicio.ts',
+  entry: './src/A18-exercicio/index.ts',
   module: {
     rules: [
       {

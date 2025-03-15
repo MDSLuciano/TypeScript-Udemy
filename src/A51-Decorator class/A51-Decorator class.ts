662aadc45347f7d2c8732b1d2c562856d043f740
Luciano Machado <lucianoo_machado@hotmail.com>
2025-03-15 16:24:33 -0300

    Introduction to Decorator

---
+++ src/A51-Decorator class/A51-Decorator class.ts
@@ -0,0 +1,23 @@
+@decorator
+export class Animal {
+  constructor(public name: string, public color: string) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T): T {
+  return class extends target {
+    color: string;
+    name: string;
+    constructor(...args: any[]) {
+      super(...args);
+      this.name = this.invert(args[0]);
+      this.color = this.invert(args[1]);
+    }
+
+    invert(value: string) {
+      return value.split('').reverse().join('');
+    }
+  };
+}
+
+const animal = new Animal('gato', 'preto');
+console.log(animal);

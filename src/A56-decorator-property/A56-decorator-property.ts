2d567b351af422ab9be268cb0e4ebf5c620638bb
Luciano Machado <lucianoo_machado@hotmail.com>
2025-03-16 17:37:25 -0300

    Decorator Property

---
+++ src/A56-decorator-property/A56-decorator-property.ts
@@ -0,0 +1,46 @@
+function decorador(classPrototype: any, name: string | symbol): any {
+  let valueProperty: any;
+  return {
+    get: () => valueProperty,
+    set: (value: any) => {
+      if (typeof value === 'string') {
+        valueProperty = value.split('').reverse().join('');
+        return;
+      }
+      return (valueProperty = value);
+    },
+  };
+}
+
+export class OnePerson {
+  @decorador
+  name: string;
+  @decorador
+  lastname: string;
+  age: number;
+  constructor(name: string, lastname: string, age: number) {
+    this.name = name;
+    this.lastname = lastname;
+    this.age = age;
+  }
+
+  method(msg: string): string {
+    return `${this.name} ${this.lastname}: ${msg}`;
+  }
+
+  get fullName(): string {
+    return `${this.name} ${this.lastname}`;
+  }
+
+  set fullName(value: string) {
+    const words = value.split(/\s+/g);
+    const firstName = words.shift();
+    if (!firstName) return;
+    this.name = firstName;
+    this.lastname = words.join(' ');
+  }
+}
+
+const person = new OnePerson('Luciano', 'Machado', 24);
+const method = person.method('Olá, Mundo!');
+console.log(method);
